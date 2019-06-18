1433cec3b85082e056079621bb8a396f0d9bc0e9
William Correa <wilcorrea@gmail.com>
2019-06-18 15:12:48 -0300

    [feature] Configurar handlers para lidar com o botão de captcha

--- index.js
+++ index.js
@@ -1,6 +1,34 @@
 require('dotenv').config()
+const TelegramBot = require('telegraf')
 
-const TelegramBot = require('telegraf');
+const lang = require('./src/lang')
+const get = require('./src/util/general').get
+const callback_query = require('./src/actions/callback_query')
+const new_chat_members = require('./src/actions/new_chat_members')
 
-const telegramBot = new TelegramBot(process.env.TELEGRAM_TOKEN);
+const bot = new TelegramBot(process.env.TELEGRAM_TOKEN)
 
+bot.use((ctx, next) => {
+  const start = new Date()
+  return next(ctx).then(() => {
+    const ms = new Date() - start
+    console.log('Response time %sms', ms)
+  })
+})
+
+/* @link https://telegraf.js.org/#/?id=update-types */
+
+// a single easter egg
+bot.hears('Hi bot', (ctx) => ctx.reply(lang('hi')))
+
+// show captcha
+bot.on('new_chat_members', new_chat_members)
+
+// solve captcha
+bot.on('callback_query', callback_query)
+
+// just show the errors
+bot.catch((err) => console.log('Ooops', err))
+
+// noinspection JSIgnoredPromiseFromCall
+bot.launch()
